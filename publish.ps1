ce18badea07b2d6864ec8df45cfe608323cfbdbd
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2019-07-23 18:12:19 +0200

    Update powershell module version on publish

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,15 @@
-# $HasMajor = $(Build.SourceVersionMessage) -like '*#major*'
-# $HasMinor = $(Build.SourceVersionMessage) -like '*#minor*'
-# $CurrentVersion = (Find-Module StepSemVer).Version
-# $Version = Step-SemVer $CurrentVersion -Major:$HasMajor -Minor:$HasMinor -Build -Revision "$(Date:yyMMdd)$(Rev:rr)"
+#Requires -Module StepSemVer
+
+if($null -ne (Find-Module StepSemVer -ErrorAction SilentlyContinue) {
+    $CurrentVersion = (Find-Module StepSemVer).Version
+    $HasMajor = $(Build.SourceVersionMessage) -like '*#major*'
+    $HasMinor = $(Build.SourceVersionMessage) -like '*#minor*'
+    $Version = Step-SemVer $CurrentVersion -Major:$HasMajor -Minor:$HasMinor -Build -Revision "$(Date:yyMMdd)$(Rev:rr)"
+
+    $File = .\StepSemVer\StepSemVer.psd1
+    $Before = 'ModuleVersion[^\n]*'
+    $After = "ModuleVersion = '$Version'"
+    (Get-Content $File -Raw) -replace $Before,$After | Set-Content $File
+}
 
 Publish-Module -Path .\StepSemVer -NuGetApiKey "$env:NUGET_API_KEY"
