4cab9b59aed34ec99065e85813577b9fecc82e03
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2019-07-23 15:44:22 +0200

    Move function to external ps1

--- StepSemVer/StepSemVer.psm1
+++ StepSemVer/StepSemVer.psm1
@@ -1,16 +1,1 @@
-function Step-SemVer {
-    param(
-        [Parameter(Position = 0, Mandatory = $true)]
-        [version]$Version,
-        [switch]$Major,
-        [switch]$Minor,
-        [switch]$Build,
-        [int]$Revision = 0
-    )
-
-    if ($true -eq $Major) { return [version]"$($Version.Major+1).0.0.$Revision" }
-    elseif ($true -eq $Minor) { return [version]"$($Version.Major).$($Version.Minor+1).0.$Revision" }
-    elseif ($true -eq $Build) { return [version]"$($Version.Major).$($Version.Minor).$($Version.Build+1).$Revision" }
-    elseif ($Revision -gt $Version.Revision) { return [version]"$($Version.Major).$($Version.Minor).$($Version.Build).$Revision" }
-    return [version]"$($Version.Major).$($Version.Minor).$($Version.Build).$($Version.Revision+1)"
-}
+Get-ChildItem $PSScriptRoot\functions -Filter *.ps1 | ForEach-Object { . $_.FullName }
