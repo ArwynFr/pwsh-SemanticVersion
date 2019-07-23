3edabbc8659781b1823558aa2093d6b6baedaa70
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2019-07-23 18:30:21 +0200

    Fix publish again

--- publish.ps1
+++ publish.ps1
@@ -6,7 +6,7 @@ if ($null -ne (Find-Module StepSemVer -ErrorAction SilentlyContinue)) {
     $HasMinor = $env:BUILD_SOURCEVERSIONMESSAGE -like '*#minor*'
     $Version = Step-SemVer $CurrentVersion -Major:$HasMajor -Minor:$HasMinor -Build -Revision $env:BUILD_BUILDNUMBER
 
-    $File = .\StepSemVer\StepSemVer.psd1
+    $File = '.\StepSemVer\StepSemVer.psd1'
     $Before = 'ModuleVersion[^\n]*'
     $After = "ModuleVersion = '$Version'"
     (Get-Content $File -Raw) -replace $Before, $After | Set-Content $File
