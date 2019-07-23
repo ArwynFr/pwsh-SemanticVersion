3e2dc47cee7c3eb2b5c4268ca2496accf9eadca5
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2019-07-23 16:47:36 +0200

    Add Pester tests output file with CI

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
 Install-Module Pester -Force
 Import-Module .\StepSemVer -Force
-Invoke-Pester .\StepSemVer\tests\*.Tests.ps1 -CodeCoverage .\StepSemVer\functions\*.ps1  -CodeCoverageOutputFile coverage.xml
+Invoke-Pester .\StepSemVer\tests\*.Tests.ps1 -CodeCoverage .\StepSemVer\functions\*.ps1  -CodeCoverageOutputFile coverage.xml -OutputFile output.xml
