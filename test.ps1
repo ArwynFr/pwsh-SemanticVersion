a8c9ef398d8b2bf5c1860068df991e100163659e
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2019-07-23 16:34:39 +0200

    Fix tests to install module

--- test.ps1
+++ test.ps1
@@ -1,4 +1,3 @@
-#Requires -Module Pester
-
+Install-Module Pester -Force
 Import-Module .\StepSemVer -Force
 Invoke-Pester .\StepSemVer\tests\*.Tests.ps1 -CodeCoverage .\StepSemVer\functions\*.ps1  -CodeCoverageOutputFile coverage.xml
