b7dadd4791b639a54c293f2b0f86cb989f8057cc
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2019-07-25 13:21:30 +0200

    Move code coverage upload to separate step

--- test.ps1
+++ test.ps1
@@ -1,9 +1,3 @@
 Install-Module Pester -Force
 Import-Module .\StepSemVer -Force
 Invoke-Pester .\StepSemVer\tests\*.Tests.ps1 -CodeCoverage .\StepSemVer\functions\*.ps1 -OutputFile output.xml -CodeCoverageOutputFile coverage.xml
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$LatestUrl = 'https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest'
-$DownloadUrl = (Invoke-WebRequest -Uri $LatestUrl | ConvertFrom-Json).assets[1].browser_download_url
-Invoke-WebRequest $DownloadUrl -OutFile codacy-coverage-reporter
-& ./codacy-coverage-reporter --language PowerShell --force-language --report coverage.xml
---
+++ upload-coverage.ps1
@@ -0,0 +1,5 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$LatestUrl = 'https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest'
+$DownloadUrl = (Invoke-WebRequest -Uri $LatestUrl | ConvertFrom-Json).assets[1].browser_download_url
+Invoke-WebRequest $DownloadUrl -OutFile codacy-coverage-reporter
+& ./codacy-coverage-reporter report --language PowerShell --force-language --report coverage.xml
