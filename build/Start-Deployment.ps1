d00d1ad55d219a0810bd2a412a43e7c202c318ba
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2024-10-03 16:08:11 +0200

    ci: fix deploy to prevent deadlocks

--- build/Start-Deployment.ps1
+++ build/Start-Deployment.ps1
@@ -1,11 +1,11 @@
 [CmdletBinding(SupportsShouldProcess)]
 param ()
 
-Import-Module ArwynFr.SemanticVersion -Force
 $RootPath = Join-Path $PSScriptRoot .. | Convert-Path
 $ModulePath = Join-Path $RootPath ArwynFr.SemanticVersion
-$TargetVersion = New-GithubSemanticVersionRelease -WhatIf:$WhatIfPreference -AddTags
+Import-Module $ModulePath -Force
 
+$TargetVersion = New-GithubSemanticVersionRelease -WhatIf:$WhatIfPreference -AddTags
 Get-ChildItem $ModulePath -Recurse -Filter *.psd1 | ForEach-Object {
   Update-ModuleManifest -ModuleVersion $TargetVersion -Path $_ -WhatIf:$WhatIfPreference
 }
