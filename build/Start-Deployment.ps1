40864abf3e6c53547213addb28503f9a1fc425d3
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2024-10-03 16:04:31 +0200

    ci: dogfooding github release (#14)
    * ci: replace deployment with lib function
    
    * chore: add final newline

--- build/Start-Deployment.ps1
+++ build/Start-Deployment.ps1
@@ -1,29 +1,13 @@
 [CmdletBinding(SupportsShouldProcess)]
 param ()
 
-
+Import-Module ArwynFr.SemanticVersion -Force
 $RootPath = Join-Path $PSScriptRoot .. | Convert-Path
 $ModulePath = Join-Path $RootPath ArwynFr.SemanticVersion
-
-Import-Module $ModulePath -Force
-$CurrentVersion = (gh release view --json tagName | ConvertFrom-Json).tagName
-$CommitMessage = [string] (git log -1 --pretty=%B)
-$Increment = $CommitMessage | ConvertTo-SemanticVersionStep
-$TargetVersion = $CurrentVersion | Step-SemanticVersion -BumpType $Increment -PreRelease $PreRelease
-
-"Increment      = $Increment" | Write-Verbose
-"CurrentVersion = $CurrentVersion" | Write-Verbose
-"TargetVersion  = $TargetVersion" | Write-Verbose
+$TargetVersion = New-GithubSemanticVersionRelease -WhatIf:$WhatIfPreference -AddTags
 
 Get-ChildItem $ModulePath -Recurse -Filter *.psd1 | ForEach-Object {
   Update-ModuleManifest -ModuleVersion $TargetVersion -Path $_ -WhatIf:$WhatIfPreference
 }
 
 Publish-Module -Path $ModulePath -NuGetApiKey $env:PSGALLERY_APIKEY -WhatIf:$WhatIfPreference
-
-if ($PSCmdlet.ShouldProcess($TargetVersion, 'gh release create')) {
-  gh release create $TargetVersion --generate-notes
-  git tag --force "v$($TargetVersion.Major).$($TargetVersion.Minor)"
-  git tag --force "v$($TargetVersion.Major)"
-  git push origin --tags --force
-}
